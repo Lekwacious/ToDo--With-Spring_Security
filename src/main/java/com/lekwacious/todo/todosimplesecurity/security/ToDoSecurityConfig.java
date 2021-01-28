3a8c04aac2f2f96bb5c4812ea38fda2ffc151a58
Victor Ihedioha <ihedioha.victor@gmail.com>
2021-01-28 02:03:55 +0100

    overiding simple security

--- src/main/java/com/lekwacious/todo/todosimplesecurity/security/ToDoSecurityConfig.java
+++ src/main/java/com/lekwacious/todo/todosimplesecurity/security/ToDoSecurityConfig.java
@@ -1,7 +1,28 @@
 package com.lekwacious.todo.todosimplesecurity.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class ToDoSecurityConfig  extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManager) throws Exception {
+        authenticationManager.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("apress")
+                .password(passwordEncoder().encode("springboot2"))
+                .roles("ADMIN", "USER");
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 }
